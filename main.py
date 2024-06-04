65f76ab847cacb9104893b7242535cfa1da5e59c
root <root@DESKTOP-1G53DPO>
2024-06-04 20:21:28 +0200

    Wczytywanie z konsoli działa i dpp działa

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 # TODO
 # 
-# - podzielic sie zadaniami
 # - sprawdzic czy wczytywanie z pliku dziala
-# - sprawdzic czy wczytywanie z konsoli dziala
-# - sprawdzic czy algorytm dziala (plecak_dynamiczny)
-# - dodac komentarze tlumaczace co robi kod
 # - clean code
-# 
 # 
 
 import argparse
 
-def plecak_dynamiczny(C, wt, val, n):
+def plecak_dynamiczny(C, wt, val, names, n):
     K = [[0 for w in range(C + 1)] for i in range(n + 1)]
     for i in range(n + 1):
         for w in range(C + 1):
@@ -21,25 +16,47 @@ def plecak_dynamiczny(C, wt, val, n):
                 K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w])
             else:
                 K[i][w] = K[i - 1][w]
-    return K[n][C]
+    
+    # Dodanie funkcji do wyświetlania przedmiotów, które znajdą się w plecaku
+    res = K[n][C]
+    w = C
+    items_in_knapsack = []
+    
+    for i in range(n, 0, -1):
+        if res <= 0:
+            break
+        if res == K[i - 1][w]:
+            continue
+        else:
+            items_in_knapsack.append(names[i - 1])
+            res = res - val[i - 1]
+            w = w - wt[i - 1]
+    
+    return K[n][C], items_in_knapsack
 
 def read_data_from_file(filename):
     with open(filename, 'r') as f:
         C = int(f.readline())
         n = int(f.readline())
-        items = [list(map(int, line.split())) for line in f]
+        names = [f.readline().strip() for _ in range(n)]  # Wczytywanie nazw przedmiotów
+        items = [list(map(int, f.readline().split())) for _ in range(n)]
         val = [item[0] for item in items]
         wt = [item[1] for item in items]
-    return C, wt, val, n
+    return C, wt, val, names, n
 
 def read_data_from_console():
     C = int(input("Podaj pojemność plecaka: "))
     n = int(input("Podaj liczbę przedmiotów: "))
-    print("Podaj wartość i wagę każdego przedmiotu (p w):")
-    items = [list(map(int, input().split())) for _ in range(n)]
-    val = [item[0] for item in items]
-    wt = [item[1] for item in items]
-    return C, wt, val, n
+    val = []
+    wt = []
+    names = []
+    for i in range(n):
+        name = input(f"Podaj nazwę przedmiotu {i+1}: ")
+        names.append(name) #TODO - JANEK - NAZWA PRZEDMIOTU MUSI ZNAJDOWAC SIE NA LISCIE PRZEDMIOTOW
+        v, w = map(int, input(f"Podaj wartość i wagę przedmiotu '{name}' (p w): ").split())
+        val.append(v)
+        wt.append(w)
+    return C, wt, val, names, n
 
 def main():
     parser = argparse.ArgumentParser()
@@ -49,10 +66,13 @@ def main():
     args = parser.parse_args()
 
     if args.file:
-        C, wt, val, n = read_data_from_file(args.file)
+        C, wt, val, names, n = read_data_from_file(args.file)
     elif args.console:
-        C, wt, val, n = read_data_from_console()
-    print(plecak_dynamiczny(C, wt, val, n))
+        C, wt, val, names, n = read_data_from_console()
+    
+    max_value, items_in_knapsack = plecak_dynamiczny(C, wt, val, names, n)
+    print(f"Maksymalna wartość, którą można umieścić w plecaku to: {max_value}")
+    print("Przedmioty, które znajdą się w plecaku to:", items_in_knapsack)
 
 if __name__ == "__main__":
     main()
